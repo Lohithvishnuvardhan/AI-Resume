6976828f8566af6433179067ed13e8b07e6bb9a0
Lohithvishnuvardhan <lohithvishnuvardhan@gmail.com>
2025-12-22 11:38:32 +0530

    Real gumroad linked

--- src/config.ts
+++ src/config.ts
@@ -12,5 +12,5 @@
 
 export const IS_PURCHASED = false; // Change to true after purchase to enable downloads
 
-export const GUMROAD_URL = 'https://lokibhai.gumroad.com/l/resumebuild';
+export const GUMROAD_URL = 'https://lokibhai.gumroad.com/l/resumebuild?_gl=1*g5m3qx*_ga*NDk3MTM1OTgwLjE3NDg5NDE2MDc.*_ga_6LJN6D94N6*czE3NjYzODMzODIkbzEyJGcxJHQxNzY2MzgzNDU0JGo1MCRsMCRoMA..';
 
